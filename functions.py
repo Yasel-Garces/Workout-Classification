bf3b76cecb95933b3172d0fbcbe4f4623b93cb4d
Yasel-Garces <88yasel@gmail.com>
2020-04-29 15:41:47 -0400

    Including a file with utilities functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 15:34:24 2020
+Utilities functions used in the project "Workout Classification"
+@author: Yasel Garces (88yasel@gmail.com)
+"""
+##------------------------------------------------------------------
+
+
+##------------------------------------------------------------------
